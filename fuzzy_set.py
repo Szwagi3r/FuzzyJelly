e3c5b9c55648d7c03b9bf5229c7a7d6135173c98
Dawid <dawid.przybylinski@gmail.com>
2021-11-18 11:38:50 +0100

    Distance diagnosis

--- fuzzy_set.py
+++ fuzzy_set.py
@@ -2,10 +2,22 @@ import numpy as np
 import pandas as pd
 
 
+class Diagnosis:
+    def __init__(self, patients, diseases, results_table, diagnosis, method):
+        self.patients = patients
+        self.diseases = diseases
+        self.results_table = results_table
+        self.diagnosis = diagnosis
+        self.method = method
+
+
 class FuzzySet:
-    def __init__(self, path):
+    def __init__(self, path, rownames=True):
         self.path = path
-        self.df = pd.read_csv(self.path)
+        if rownames:
+            self.df = pd.read_csv(self.path, index_col=0)
+        else:
+            self.df = pd.read_csv(self.path)
         membership = []
         non_membership = []
         for i in range(self.df.shape[1]):
@@ -15,6 +27,16 @@ class FuzzySet:
                 non_membership.append(self.df.iloc[:, i].to_numpy())
         self.membership = membership
         self.non_membership = non_membership
+        df2 = []
+        for i in range(len(membership[0])):
+            new_obs = []
+            for j in range(len(membership)):
+                new_obs.append([membership[j][i], non_membership[j][i]])
+            df2.append(new_obs)
+        df_compressed = pd.DataFrame(df2)
+        df_compressed.columns = [self.df.columns[2*i] for i in range(len(membership))]
+        df_compressed.index = self.df.index
+        self.df_compressed = df_compressed
 
     def similarity(self, other):
         assert isinstance(other, FuzzySet)
@@ -40,18 +62,48 @@ class FuzzySet:
             denominator += max(np.dot(self.non_membership[i], self.non_membership[i]), np.dot(other.non_membership[i], other.non_membership[i]))
         return numerator/denominator/np.sum(weights)
 
+    def distance_diagnosis(self, patients, dist_type="euclidean"):
+        def distance(disease, patient, dist_type):
+            result = 0
+            if dist_type == "euclidean":
+                for i in range(len(patient)):
+                    result += (disease[i][0] - patient[i][0])**2+(disease[i][1] - patient[i][1])**2+(disease[i][0] - patient[i][0] + disease[i][1] - patient[i][1])**2
+                return np.sqrt(result)
+            if dist_type == "absolute":
+                for i in range(len(patient)):
+                    result += (disease[i][0] - patient[i][0])**2+(disease[i][1] - patient[i][1])**2+(disease[i][0] - patient[i][0] + disease[i][1] - patient[i][1])**2
+                return np.sqrt(result)
+
+        if dist_type not in ["euclidean", "absolute"]:
+            print("Wrong distance type, using euclidean instead.")
+            dist_type = "euclidean"
+        distances = []
+        for P_id in range(patients.df_compressed.shape[0]):
+            patient_distances = []
+            for D_id in range(self.df_compressed.shape[1]):
+                patient = patients.df_compressed.iloc[P_id, :]
+                disease = self.df_compressed.iloc[:, D_id]
+                patient_distances.append(distance(disease, patient, dist_type))
+            distances.append(patient_distances)
+        df_distances = pd.DataFrame(distances)
+        df_distances.columns = self.df_compressed.columns
+        df_distances.index = patients.df_compressed.index
+        diagnosis = []
+        for P_id in range(df_distances.shape[0]):
+            diagnosis.append([df_distances.index[P_id], df_distances.columns[np.argmin(df_distances.iloc[P_id, :])]])
+        return Diagnosis(patients.df_compressed, self.df_compressed, df_distances, pd.DataFrame(diagnosis, columns=["patient", "disease"]), method=dist_type+" distance")
+
 
 if __name__ == '__main__':
-    A = FuzzySet("data\set_1.csv")
-    print(A.membership)
-    print(A.non_membership)
-    B = FuzzySet("data\set_2.csv")
-    print(B.membership)
-    print(B.non_membership)
-    print(A.similarity(B))
-    print(A.weighted_similarity(B, weights=[0.5, 0.6, 0.8]))
-    M = FuzzySet("data\disease_1_M.csv")
-    P1 = FuzzySet("data\disease_1_P1.csv")
-    print(P1.membership)
-    print(P1.non_membership)
-    print(P1.similarity(M))
+    A = FuzzySet("data\patients.csv")
+    B = FuzzySet("data\diseases.csv")
+    d = B.distance_diagnosis(A, dist_type="e")
+    print(d.diagnosis)
+    print(d.method)
+
+    #M = FuzzySet("data\disease_1_M.csv", False)
+    #print(M.df_compressed)
+    #P1 = FuzzySet("data\disease_1_P1.csv")
+    #print(P1.membership)
+    #print(P1.non_membership)
+    #print(P1.similarity(M))
